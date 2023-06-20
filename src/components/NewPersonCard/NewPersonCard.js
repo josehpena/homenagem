625d7ee4e65ef64e72b5ff8edf414c88f326dd28
Jose Pena <josehpena2@gmail.com>
2023-06-20 15:31:12 -0300

    css

--- src/components/NewPersonCard/NewPersonCard.js
+++ src/components/NewPersonCard/NewPersonCard.js
@@ -1,20 +1,39 @@
 import React, { useContext } from 'react';
 import { PersonCardsContext } from '../../store/PersonCardsProvider';
+import './NewPersonCard.css';
 
 const NewPersonCard = () => {
+  const PersonCardsCtx = useContext(PersonCardsContext);
 
-    const PersonCardsCtx = useContext(PersonCardsContext);
-    
-    return (
-        <form onSubmit={PersonCardsCtx.addPersonCard}>
-            <input type='text' name='name' placeholder='Nome da pessoa' />
-            <input type='text' name='biography' placeholder='Biografia' />
-            <input type='text' name='birthDate' placeholder='Data de nascimento' />
-            <input type='text' name='deathDate' placeholder='Data da morte' />
-            <input type='text' name='photo' placeholder='Imagem da pessoa' />
-            <input type='submit' value='Adicionar nova Homenagem' />
-        </form>
-    );
-}
+  return (
+    <div className="NewPersonCardContainer">
+      <form className="NewPersonCardForm" onSubmit={PersonCardsCtx.addPersonCard}>
+        <div className="form-group">
+          <label htmlFor="name">Nome da pessoa</label>
+          <input type="text" id="name" name="name" placeholder="Nome da pessoa" />
+        </div>
+        <div className="form-group">
+          <label htmlFor="biography">Biografia</label>
+          <input type="text" id="biography" name="biography" placeholder="Biografia" />
+        </div>
+        <div className="form-group">
+          <label htmlFor="birthDate">Data de nascimento</label>
+          <input type="text" id="birthDate" name="birthDate" placeholder="Data de nascimento" />
+        </div>
+        <div className="form-group">
+          <label htmlFor="deathDate">Data da morte</label>
+          <input type="text" id="deathDate" name="deathDate" placeholder="Data da morte" />
+        </div>
+        <div className="form-group">
+          <label htmlFor="photo">Imagem da pessoa</label>
+          <input type="text" id="photo" name="photo" placeholder="Imagem da pessoa" />
+        </div>
+        <button type="submit" className="submit-button">
+          Adicionar nova Homenagem
+        </button>
+      </form>
+    </div>
+  );
+};
 
 export default NewPersonCard;
--- src/components/PersonCards/PersonCards.js
+++ src/components/PersonCards/PersonCards.js
@@ -2,26 +2,26 @@ import React, { useContext } from 'react';
 import PersonCard from '../PersonCard';
 import { PersonCardsContext } from '../../store/PersonCardsProvider';
 import { UsersContext } from '../UserProvider/UserProvider';
+import './PersonCards.css';
 
 const PersonCards = () => {
   const personCardsCtx = useContext(PersonCardsContext);
-  const {authenticated} = useContext(UsersContext);
+  const { authenticated } = useContext(UsersContext);
 
   if (!personCardsCtx.personCards || personCardsCtx.personCards.length === 0) {
     return <div>Não há cartões de pessoa disponíveis.</div>;
   }
 
   return (
-    <>
-    {
-        authenticated ? 
-        <>
-      {personCardsCtx.personCards.map((personCard) => (
-        <PersonCard key={personCard.id} personCard={personCard} />
-      ))}</>:(
-      <p>Usuário não autenticado</p>)
-      }
-    </>
+    <div className="PersonCards">
+      {authenticated ? (
+        personCardsCtx.personCards.map((personCard) => (
+          <PersonCard key={personCard.id} personCard={personCard} />
+        ))
+      ) : (
+        <p>Usuário não autenticado</p>
+      )}
+    </div>
   );
 };
 
