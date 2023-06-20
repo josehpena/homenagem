d19dab64bd18bb1e56d4303c6a9589a3bbba39de
Jose Pena <josehpena2@gmail.com>
2023-06-20 16:09:42 -0300

    Verificacoes

--- src/components/NewPersonCard/NewPersonCard.js
+++ src/components/NewPersonCard/NewPersonCard.js
@@ -1,32 +1,75 @@
 import React, { useContext } from 'react';
+import { useForm } from 'react-hook-form';
 import { PersonCardsContext } from '../../store/PersonCardsProvider';
 import './NewPersonCard.css';
 
 const NewPersonCard = () => {
   const PersonCardsCtx = useContext(PersonCardsContext);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data, event) => {
+    event.preventDefault();
+    PersonCardsCtx.addPersonCard(data);
+  };
 
   return (
     <div className="NewPersonCardContainer">
-      <form className="NewPersonCardForm" onSubmit={PersonCardsCtx.addPersonCard}>
+      <form className="NewPersonCardForm" onSubmit={handleSubmit(onSubmit)}>
         <div className="form-group">
           <label htmlFor="name">Nome da pessoa</label>
-          <input type="text" id="name" name="name" placeholder="Nome da pessoa" />
+          <input
+            type="text"
+            id="name"
+            name="name"
+            placeholder="Nome da pessoa"
+            {...register('name', { required: true, maxLength: 100 })}
+          />
+          {errors.name && <span className="error-message">Este campo é obrigatório.</span>}
         </div>
         <div className="form-group">
           <label htmlFor="biography">Biografia</label>
-          <input type="text" id="biography" name="biography" placeholder="Biografia" />
+          <input
+            type="text"
+            id="biography"
+            name="biography"
+            placeholder="Biografia"
+            {...register('biography', { required: true, maxLength: 1000 })}
+          />
+          {errors.biography && <span className="error-message">Este campo é obrigatório.</span>}
         </div>
         <div className="form-group">
           <label htmlFor="birthDate">Data de nascimento</label>
-          <input type="text" id="birthDate" name="birthDate" placeholder="Data de nascimento" />
+          <input
+            type="text"
+            id="birthDate"
+            name="birthDate"
+            placeholder="Data de nascimento"
+            {...register('birthDate')}
+          />
         </div>
         <div className="form-group">
           <label htmlFor="deathDate">Data da morte</label>
-          <input type="text" id="deathDate" name="deathDate" placeholder="Data da morte" />
+          <input
+            type="text"
+            id="deathDate"
+            name="deathDate"
+            placeholder="Data da morte"
+            {...register('deathDate')}
+          />
         </div>
         <div className="form-group">
           <label htmlFor="photo">Imagem da pessoa</label>
-          <input type="text" id="photo" name="photo" placeholder="Imagem da pessoa" />
+          <input
+            type="text"
+            id="photo"
+            name="photo"
+            placeholder="Imagem da pessoa"
+            {...register('photo')}
+          />
         </div>
         <button type="submit" className="submit-button">
           Adicionar nova Homenagem
--- src/store/PersonCardsProvider.js
+++ src/store/PersonCardsProvider.js
@@ -2,42 +2,46 @@ import React, { useState, useEffect } from 'react';
 import { addDoc, collection } from 'firebase/firestore';
 import { db } from './FirebaseConfig';
 import { getDocs } from 'firebase/firestore';
+import { useForm } from 'react-hook-form';
 
 export const PersonCardsContext = React.createContext();
 
 const PersonCardsProvider = (props) => {
   const [personCards, setPersonCards] = useState([]);
+  const {
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
   useEffect(() => {
     fetchPersonCards();
   }, []);
 
-  async function addPersonCard(event) {
-    event.preventDefault();
+  const addPersonCard = async (data) => {
     try {
       let newPersonCard = {
         id: personCards.length + 1,
-        name: event.target.name.value,
-        biography: event.target.biography.value,
-        birthDate: event.target.birthDate.value,
-        deathDate: event.target.deathDate.value,
-        photo: event.target.photo.value,
+        name: data.name,
+        biography: data.biography,
+        birthDate: data.birthDate,
+        deathDate: data.deathDate,
+        photo: data.photo,
       };
       await addDoc(collection(db, 'homenagens'), newPersonCard);
       fetchPersonCards(); // Atualiza os cards após adicionar um novo
     } catch (e) {
       console.error('Error adding document: ', e);
     }
-  }
+  };
 
   const fetchPersonCards = async () => {
     try {
       const querySnapshot = await getDocs(collection(db, 'homenagens'));
       const personCardsAux = [];
-      querySnapshot.forEach((doc) =>{
-        const {...data} = doc.data();
+      querySnapshot.forEach((doc) => {
+        const { ...data } = doc.data();
         personCardsAux.push({ id: doc.id, ...data });
-    });
+      });
       setPersonCards(personCardsAux);
       console.log(personCardsAux);
     } catch (error) {
@@ -47,7 +51,12 @@ const PersonCardsProvider = (props) => {
 
   return (
     <PersonCardsContext.Provider
-      value={{ personCards: personCards, addPersonCard: addPersonCard }}
+      value={{
+        personCards: personCards,
+        addPersonCard: addPersonCard,
+        handleSubmit: handleSubmit,
+        errors: errors,
+      }}
     >
       {props.children}
     </PersonCardsContext.Provider>
