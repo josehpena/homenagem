f3956c699fba7736b9d53bfb722a8b6772809473
Jose Pena <josehpena2@gmail.com>
2023-06-27 00:01:29 -0300

    Header com authenticated

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,26 +1,45 @@
 import React, { useContext } from 'react';
 import { NavLink } from 'react-router-dom';
-import { AuthContext } from '../../store/AuthProvider';
+import { UsersContext } from '../UserProvider/UserProvider';
 
 import './Header.css';
 
 const Header = () => {
-    const authContext = useContext(AuthContext);
-    const { isAuthenticated } = authContext;
-    return (
-        <header className="Header">
-            <nav>
-                <ul>
-                {isAuthenticated && (
-                <NavLink to="/" activeClassName="active">Homenagens</NavLink>
-                )}
-                <NavLink to="/novaHomenagem" activeClassName="active">Nova Homenagem</NavLink>
-                <NavLink to="/login" activeClassName="active">Login</NavLink>
-                <NavLink to="/faleConosco" activeClassName="active">Fale Conosco</NavLink>
-                </ul>
-            </nav>
-        </header>
-    );
+  const { authenticated } = useContext(UsersContext);
+
+  return (
+    <header className="Header">
+      <nav>
+        <ul>
+          {authenticated && (
+            <li>
+              <NavLink to="/" activeClassName="active">
+                Homenagens
+              </NavLink>
+            </li>
+          )}
+          {authenticated && (
+            <li>
+              <NavLink to="/novaHomenagem" activeClassName="active">
+                Nova Homenagem
+              </NavLink>
+            </li>
+          )}
+          <li>
+            <NavLink to="/faleConosco" activeClassName="active">
+              Fale Conosco
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/login" activeClassName="active">
+              Login
+            </NavLink>
+          </li>
+          
+        </ul>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
